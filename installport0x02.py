d2e50dc43cd4bab14ff03dfa0906f2ecc93589fa
Team0x02 <119530796+Team0x02@users.noreply.github.com>
2022-12-11 01:54:25 -0800

    Termux Port Tarama Aracı
    python3 installport0x02.py

---
+++ installport0x02.py
@@ -0,0 +1,150 @@
+#kurulum-araci
+#port-tarama
+
+
+
+
+print("""\033[34m OTOMATİK SİSTEM KURAN PORT ARACINA HOŞGELDİNİZ\033[97m
+""")
+enter=input("\033[34mDevam etmek için enter basın :\033[97m")
+print("""\033[34m
+
+  __  __ __    _  _  _   _   __    _   _   _       _   _   _  __
+ / _ \ \/ / _ \___ \  | |/ / | | | | |  _ \  | | | | | |     | | | | |  \/  |
+| | | \  / | | |_) | | ' /  | | | | | |) | | | | | | |     | | | | | |\/| |
+| || /  \ || / _/  | . \  | || | |  _ <  | || | | |_  | || | | |  | |
+ \__//\_\__/_| ||\_\  \__/  || \_\  \__/  |_|  \_/  ||  |_| 1.0\033[97m
+""")
+
+
+print("\033[34m[✔] https://github.com/Team0x02/\033[97m")
+print("""
+•••••••
+•••••••
+•••••••
+•••••••""")
+
+import os
+
+os.system("pkg install python")
+os.system("pip install whois")
+os.system("pkg install nmap -y")
+os.system("apt install wifite -y")
+os.system("apt install php -y")
+os.system("pip install mechanize")
+os.system("pip install json")
+os.system("pip install whois")
+os.system("pip install python-whois")
+os.system("pip install requests")
+os.system("pip install bs4")
+os.system("pip install tkinter")
+os.system("pip install urlparse")
+os.system("pip install cookielib")
+os.system("pip install scapy")
+os.system("pip install datetime")
+os.system("pip install argparse")
+os.system("pip install re")
+os.system("pip install threading")
+os.system("pip install urllib2")
+os.system("pip install modules")
+os.system("pip install builtwith")
+os.system("pip install smtplib")
+os.system("pip install whois")
+os.system("pip install builtwith")
+os.system("pip install colorama")
+os.system("pip install dnspython")
+os.system("pip install shodan")
+os.system("clear")
+print("KURULUM DEVAM EDİYOR")
+print("++++++++++++++++++++++")
+print("%25")
+print("++++++++++++++++++++++")
+print("%50")
+print("++++++++++++++++++++++")
+print("%75")
+print("++++++++++++++++++++++")
+print("%100")
+
+import os
+
+os.system("pkg install perl")
+os.system("pkg install vim")
+os.system("pkg install pip")
+os.system("pkg install pip2")
+os.system("pip install wordlist")
+os.system("pkg install openssl")
+os.system("apt install nodejs")
+os.system("pip install colorama")
+os.system("pip install requests")
+os.system("apt install ping")
+os.system("pkg install python2")
+os.system("pkg install python3")
+os.system("apt install cmatrix")
+os.system("apt install dnsutils")
+os.system("apt install hydra")
+os.system("apt install ninja")
+os.system("apt install sslscan")
+os.system("apt install unzip")
+os.system("apt install termux-tools")
+os.system("apt install macchanger -y")
+os.system("apt install busybox")
+
+
+import os
+
+os.system("apt-get install figlet")
+os.system("clear")
+os.system("figlet 0x02")
+os.system("figlet PORT TARAMA")
+
+print("EXTRA OLARAK PORT TARAMA TOOLU;")
+
+print("\033[34m[✔] https://github.com/Team0x02/\033[97m")
+
+print("""
+1) Hızlı Port Tarama
+2) Servis ve Versiyon bilgisi
+3) Isletim Sistemi Bilgisi
+    """)
+
+islemno=input("Islem Numarası giriniz : ")
+
+if islemno=="1":
+        hedefip=input("Hedef ip giriniz : ")
+        os.system("nmap "+hedefip)
+elif islemno=="2":
+        hedefip=input("Hedef ip giriniz : ")
+        os.system("nmap -sS -sV "+hedefip)
+elif islemno=="3":
+        hedefip=input("Hedef ip giriniz : ")
+        os.system("nmap -0 "+hedefip)
+else:
+        print("Yanlış tuşa bastınız...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("""
+•
+•
+•
+•
+•
+""")
+
+
+print("TAMAMLANDI!")
+print("\033[34m[✔] https://github.com/Team0x02/\033[97m")
+print("""İLETİŞİM İÇİN İNSTAGRAM
+@ghost0x02
+@readlydex0x02
+""")
